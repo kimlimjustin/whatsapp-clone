a77b6ab9f916e750d3150c3673b70b6b46dd425f
kimlimjustin <kimlimjustin@gmail.com>
2020-11-01 21:09:13 +0700

    add secretkey to gitignore

--- client/src/Components/home.component.js
+++ client/src/Components/home.component.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Cookies from 'universal-cookie';
+import getUserByToken from '../Library/getUserByToken';
 
 const Home = () => {
+    const [userInfo, setUserInfo] = useState({});
+
+    useEffect(() => {
+        const token = new Cookies().get('token');
+        getUserByToken(token).then(res => {
+            if(res.status) window.location = "/login";
+            else {
+                setUserInfo(res);
+                const cookie = new Cookies();
+                cookie.set('token', res.token, {path: '/', maxAge:604800 })
+            }
+        })
+    }, [])
     return(
-        <div>Home Page</div>
+        <div className = "container">
+
+        </div>
     )
 }
 
--- client/src/Static/SecretKey.static.js
+++
@@ -1,2 +0,0 @@
-const SECURITY_KEY = '9dQ)_ocEEW2th1,Jq|eYKKTNc|(&7E8qIBqlJY~5l}(,]Ag.~*#kViG,hhJ=Q^)';
-export default SECURITY_KEY;
